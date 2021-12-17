4e88eeb94ba6455490957aa5097802028fa6bb80
Jacko0b <jakub.sobota.pl@gmail.com>
2021-12-17 18:02:54 +0100

    Added basic HUD,  Questions logic

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion ={};
 let availableQuestions=[];
-let acceptingAnswers =true;
+let acceptingAnswers =false;
 let questionCounter=0;
 let score=0;
 
@@ -43,14 +45,65 @@ startGame=()=>{
     questionCounter=0;
     score=0;
     availableQuestions =[...questions];
-    console.log(availableQuestions);
     getNewQuestion();
 };
 
 getNewQuestion = () => {
+
+    if(availableQuestions.length === 0 || questionCounter>= MAX_QUESTIONS){
+// go to end page
+    return window.location.assign("/end.html");
+    }
+
     questionCounter++;
+    // questionCounterText.innerText = questionCounter + "/" +MAX_QUESTIONS;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice'+number];
+    });
+
+    availableQuestions.splice(questionIndex,1);
+
+    acceptingAnswers=true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e =>{
+        if(!acceptingAnswers)return;
+
+        acceptingAnswers=false;
+
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        // const classToApply = 'incorrect';
+        // if(selectedAnswer == currentQuestion.answer){
+        //     classToApply='correct';
+        // }
+        //the same as below
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if(classToApply==='correct'){
+            incrementScore(CORRECT_BONUS);
+        }
+        
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+ 
+    });
+});
+
+incrementScore = num =>{
+    score +=num;
+    scoreText.innerText=score;
 }
+
 startGame();
