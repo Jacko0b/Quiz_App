37ea56938b51c74d8ca4424a41e2a3d17a2b907d
Jacko0b <jakub.sobota.pl@gmail.com>
2021-12-17 18:59:14 +0100

    EndGameScreen
    Added end game screen elements.
    Score is saved in localStorage and displayed after game, save button is locked untill you pass username.

--- game.js
+++ game.js
@@ -52,8 +52,9 @@ startGame=()=>{
 getNewQuestion = () => {
 
     if(availableQuestions.length === 0 || questionCounter>= MAX_QUESTIONS){
-// go to end page
-    return window.location.assign("/end.html");
+        localStorage.setItem('mostRecentScore', score);
+        // go to end page
+        return window.location.assign("/end.html");
     }
 
     questionCounter++;
